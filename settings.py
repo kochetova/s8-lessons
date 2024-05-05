c67770fc07d5614e415ab457c512df80867500ae
Dasha Kochetova, junior de <win.girl23@gmail.com>
2024-05-05 05:13:45 -0600

    try

--- settings.py
+++ settings.py
@@ -1,2 +1,6 @@
-USER_HOST = 'xx.xx.xx.xx' # укажите свой хост
-TOPIC_NAME = 'student.topic.cohort<номер когорты>.<username>' # укажите название топика student.topic.cohort<номер когорты>.<username>
+USER_HOST = '178.154.207.86' # укажите свой хост
+TOPIC_NAME = 'student.topic.cohort21.ddkochetova' # укажите название топика student.topic.cohort<номер когорты>.<username>
+
+#docker run -it --network=rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091 edenhill/kcat:1.7.1 -X security.protocol=SASL_SSL -X sasl.mechanisms=SCRAM-SHA-512 -X sasl.username=de-student -X sasl.password=ltcneltyn -X ssl.ca.location=/lessons/CA.pem
+
+#kafkacat -b rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091 -X security.protocol=SASL_SSL -X sasl.mechanisms=SCRAM-SHA-512 -X sasl.username="de-student" -X sasl.password="ltcneltyn" -X ssl.ca.location=/usr/local/share/ca-certificates/Yandex/YandexCA.crt -t base -K: -P -l /lessons/your-folder/old-yet-gold.txt
--- Тема 3. Настройка потока данных/4. Изучение данных в Kafka со Spark/Задание 2/realization.py
+++ Тема 3. Настройка потока данных/4. Изучение данных в Kafka со Spark/Задание 2/realization.py
@@ -1,30 +1,49 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f, DataFrame
-from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType
+from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType, LongType, TimestampType
 
 # необходимая библиотека с идентификатором в maven
 # вы можете использовать ее с помощью метода .config и опции "spark.jars.packages"
-kafka_lib_id =
-        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
+spark_jars_packages = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
 
 # настройки security для кафки
 # вы можете использовать из с помощью метода .options(**kafka_security_options)
 kafka_security_options = {
     'kafka.security.protocol': 'SASL_SSL',
+    'kafka.bootstrap.servers': 'rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091',
     'kafka.sasl.mechanism': 'SCRAM-SHA-512',
     'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"de-student\" password=\"ltcneltyn\";',
+    'subscribe': 'persist_topic',
 }
+# определяем схему входного сообщения для JSON
+incomming_message_schema = StructType(
+    [StructField("subscription_id", IntegerType(), True),
+     StructField("name" , StringType(), True),
+     StructField("description" , StringType(), True),
+     StructField("price", DoubleType(), True),
+     StructField("currency" , StringType(), True),
+     StructField("key" , StringType(), True),
+     StructField("value", StringType(), True),
+     StructField("topic" , StringType(), True),
+     StructField("partition" , IntegerType(), True),
+     StructField("offset", LongType(), True),
+     StructField("timestamp" , TimestampType(), True),
+     StructField("timestampType" , IntegerType(), True)])
 
 def spark_init() -> SparkSession:
-    pass
+    return SparkSession.builder \
+        .appName("persist_topic") \
+        .config("spark.jars.packages", spark_jars_packages) \
+        .getOrCreate()
 
 
 def load_df(spark: SparkSession) -> DataFrame:
-    pass
+    return spark.read.format('kafka').options(**kafka_security_options).load()
 
 
 def transform(df: DataFrame) -> DataFrame:
-    pass
+    return (df.select(f.from_json(f.col("value").cast("string"), incomming_message_schema).alias("parsed_key_value"))
+            .select(f.col("parsed_key_value.*")))
 
 
 spark = spark_init()
